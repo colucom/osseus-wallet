3ec5a0491d57932e18383312ff2ba4851dc5ee9b
leonprou <leonp@colu.com>
2018-04-11 13:17:35 +0300

    allow create new contracts

--- examples/contract.js
+++ examples/contract.js
@@ -11,13 +11,23 @@ async function main () {
 
   try {
     console.log(`account: ${web3.eth.personal.defaultAccount}`)
-    const ColuLocalNetworkContract = contract.getContract('ColuLocalNetwork')
+    const ColuLocalNetworkContract = contract.getContract({contractName: 'ColuLocalNetwork'})
 
     const name = await ColuLocalNetworkContract.methods.name().call()
     console.log(`contract name: ${name}`)
 
     const balance = await ColuLocalNetworkContract.methods.balanceOf(config.address).call()
     console.log(`account balance: ${balance}`)
+
+    const CurrencyFactoryContract = contract.getContract({contractName: 'CurrencyFactory'})
+    const tokenAddress = await CurrencyFactoryContract.methods.tokens(0).call()
+
+    const ColuLocalCurrencyContract = contract.getContract({abiName: 'ColuLocalNetwork', address: tokenAddress})
+    const ccName = await ColuLocalCurrencyContract.methods.name().call()
+    console.log(`Colu Local Currency name: ${ccName}`)
+
+    contract.addContract({contractName: ccName, contract: ColuLocalCurrencyContract})
+    console.log('new contract have been added to the contracts store')
 
     console.log('approving 1 CLN, this call might hang up')
     const receipt = await ColuLocalNetworkContract.methods.approve('0x0d4DF041Dbef6fFC0E444a4a213774AdB0c118C2', 1 ** 18).send({
